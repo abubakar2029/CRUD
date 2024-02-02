3692d98fcabf4e5d0f713715b920b3aa4b081e6c
Abu Bakar <abubakarzafar2029@gmail.com>
2024-02-02 14:46:25 +0500

    some changes

--- Rest_Api + JWT/server/src/api/users.js
+++ Rest_Api + JWT/server/src/api/users.js
@@ -5,12 +5,9 @@ var jwt = require("jsonwebtoken");
 
 router.post("/signup", async function (req, res) {
   const user = new User(req.body);
-  console.log(req.body);
   const response = await user.save();
-  let token = await jwt.sign(req.body, "secret");
-  console.log(token);
-  console.log(" ");
   console.log("Ya reponse aya", response._id);
+  let token = await jwt.sign(req.body, "secret");
   res.send({ status: 200, user: token });
 });
 
